53aa79dd599eebfa2874f6154c53dfc376c7cab5
Lukols-Dev <lukols.dev@gmail.com>
2024-01-10 17:46:54 +0100

    Component: Magnetic.tsx fix problem with types in current element and cloneElemetn children

--- src/components/common/Magnetic/index.tsx
+++ src/components/common/Magnetic/index.tsx
@@ -1,41 +1,52 @@
 "use client";
 
-import { FC, ReactNode, cloneElement, useEffect, useRef } from "react";
+import { FC, ReactElement, cloneElement, useEffect, useRef } from "react";
 import gsap from "gsap";
 
 interface MagneticProps {
-  children: ReactNode;
+  children: ReactElement;
 }
 
 const Magnetic: FC<MagneticProps> = ({ children }) => {
-  const magnetic = useRef(null);
+  const magnetic = useRef<HTMLElement>(null);
   useEffect(() => {
-    const xTo = gsap.quickTo(magnetic.current, "x", {
+    const element = magnetic.current;
+    if (!element) return;
+
+    const xTo = gsap.quickTo(element, "x", {
       duration: 1,
       ease: "elastic.out(1, 0.3)",
     });
-    const yTo = gsap.quickTo(magnetic.current, "y", {
+
+    const yTo = gsap.quickTo(element, "y", {
       duration: 1,
       ease: "elastic.out(1, 0.3)",
     });
-    //@ts-ignore
-    magnetic.current.addEventListener("mousemove", (e) => {
+
+    const handleMouseMove = (e: any) => {
       const { clientX, clientY } = e;
-      const { height, width, left, top } =
-        //@ts-ignore
-        magnetic.current.getBoundingClientRect();
+      const { height, width, left, top } = element.getBoundingClientRect();
       const x = clientX - (left + width / 2);
       const y = clientY - (top + height / 2);
       xTo(x * 0.35);
       yTo(y * 0.35);
-    });
-    //@ts-ignore
-    magnetic.current.addEventListener("mouseleave", (e) => {
+    };
+
+    const handleMouseLeave = () => {
       xTo(0);
       yTo(0);
-    });
+    };
+
+    element.addEventListener("mousemove", handleMouseMove);
+    element.addEventListener("mouseleave", handleMouseLeave);
+
+    // remove event listeners, so that there is no memory leakage
+    return () => {
+      element.removeEventListener("mousemove", handleMouseMove);
+      element.removeEventListener("mouseleave", handleMouseLeave);
+    };
   }, []);
-  //@ts-ignore
+
   return cloneElement(children, { ref: magnetic });
 };
 
